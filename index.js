dd3accf80e1d4dd17675945ee5724a57c49dba3f
Nolsters <nolanhepworth@gmail.com>
2020-10-06 17:13:18 -0400

    Added Music Bot feature

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const prefix = '!';
 
 const fs = require('fs');
 
+var servers = {};
+
+const ytdl = require("ytdl-core");
+
 client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
@@ -55,5 +59,73 @@ client.on('message', message =>{
 
 
 
-client.login('NzU1NTA4NDU2NDcxMTM0MjI4.X2EUFA.cFhHUHnoutxiGlC4kqB3pkaIflo');
 
+client.on('message',message =>{
+
+    let args = message.content.substring(prefix.length).split(" ");
+    switch(args[0]){
+        case 'play':
+            
+            function play(connection,message){
+                var server = servers[message.guild.id];
+
+                server.dispatcher = connection.play(ytdl(server.queue[0],{filter: 'audioonly'}));
+                server.queue.shift();
+                server.dispatcher.on("end",function(){
+                    if(server.queue[0]){
+                        play(connection,message);
+                    } else {
+                        connection.disconnect();
+                    }
+                });
+
+            }
+
+            if(!args[1]){
+                message.channel.send("you need to provide a link");
+                return;
+            }
+            if(!message.member.voice.channel){
+                message.channel.send("you must be in a channel to play the bot !");
+                return;
+            }
+
+            if(!servers[message.guild.id]) servers[message.guild.id]={
+                queue :[]
+            }
+            var server = servers[message.guild.id];
+
+            server.queue.push(args[1]);
+
+            if(!message.guild.voiceConnection) 
+            message.member.voice.channel.join().then(function(connection){
+                play(connection,message);
+            })
+
+            break;
+        case 'skip':
+                var server = servers[message.guild.id];
+                if(server.dispatcher) server.dispatcher.end();
+                message.channel.send("song skipped")
+        break;
+
+        case 'stop':
+                console.log('Testing the case');
+                var server = servers[message.guild.id];
+                if(message.guild.voiceConnection){
+                    for(var i=server.queue.length -2;i>=0;i--){
+                        server.queue.splice(i,1);
+                    }
+                    console.log('passed if loop')
+                    server.dispatcher.end();
+                    message.channel.send("Ending the queue leaving the voice channel")
+                    console.log('stopped the queue')
+                }
+                if(message.guild.connection) message.member.voice.channel.leave();
+        
+        break;
+
+    }
+})
+
+client.login('NzU1NTA4NDU2NDcxMTM0MjI4.X2EUFA.aG4HYdAf0avRr_zkIi4kUORsM44');
